c0ad47a878e6f1b1b673a69c1b503602890e223e
Abhijeet Tiwari <abhijeett709@gmail.com>
2024-10-29 02:21:47 +0530

    Addition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React,{useState} from 'react';
+import "./App.css";
 
 function App() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [sum, setSum] = useState(0);
+
+  const addition = () => {
+    const num1 = parseFloat(number1);
+    const num2 = parseFloat(number2);
+    setSum(num1 + num2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Add Two Number</h1>
+      <input
+        type="number"
+        placeholder="Enter First number"
+        value={number1}
+        onChange={(e) => setNumber1(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Enter Second number"
+        value={number2}
+        onChange={(e) => setNumber2(e.target.value)}
+      />
+      <button onClick={addition}> Add </button>
+      <h2>Sum : {sum}</h2>
     </div>
   );
 }
